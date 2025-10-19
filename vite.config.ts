2239fc9bb074c6650747338eaa6f149b7a5b2962
NevezDev <serginho-nvs@hotmail.com>
2025-10-19 17:46:22 -0300

    Primeiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -15,7 +15,7 @@ export default defineConfig({
     host: true,
   },
   preview: {
+    allowedHosts: ['nuvra-nuvracrm.3kuf6w.easypanel.host'],
     host: true,
-    allowedHosts: ['nuvra-nuvracrm.3kuf6w.easypanel.host', '.easypanel.host'],
   },
 })
---
+++ vite.preview.config.js
@@ -0,0 +1,6 @@
+export default {
+  preview: {
+    allowedHosts: ['nuvra-nuvracrm.3kuf6w.easypanel.host'],
+    host: true,
+  },
+};
